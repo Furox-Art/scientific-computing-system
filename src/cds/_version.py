477ac8e79c6ad9637fae749a0ddc2ea4c3eb5b6c
Furox <real.furkannn@gmail.com>
2026-09-05 01:41:03 +0300

    chore(release): sync package version 1.8.0

--- src/cds/_version.py
+++ src/cds/_version.py
@@ -7,5 +7,5 @@ from __future__ import annotations
 
 __all__ = ["__version__", "version", "__version_tuple__", "version_tuple"]
 
-__version__ = version = "1.7.3"
-__version_tuple__ = version_tuple = (1, 7, 3)
+__version__ = version = "1.8.0"
+__version_tuple__ = version_tuple = (1, 8, 0)
